ad295b1a3508dff808be1964d7ea50e55cca100b
Rama <ramanathanpri@gmail.com>
2021-10-01 19:32:19 +0530

    CSS Changes Added

--- narenphotography/src/components/GalleryComponent.js
+++ narenphotography/src/components/GalleryComponent.js
@@ -6,15 +6,22 @@ import { Link } from 'react-router-dom';
 
     function RenderBusItem({ bus, onClick }) {
         return(
-            <Link to={`gallery/${bus.id}`} >
-            <Card>
-                <CardImg width="100%" src={bus.image} alt={bus.name} />
-                <CardBody>
-                    <CardTitle> {bus.name}</CardTitle>
+            
+            <Card className="flip-card-inner">
+                <div className="flip-card-front">
+                    <CardImg width="100%" src={bus.image} alt={bus.name} />
+                </div>
+               
+                <CardBody className="flip-card-back">
+                    <Link to={`gallery/${bus.id}`} >
+                         <h6>View More Pics</h6>
+                         <CardTitle> {bus.name}</CardTitle>
+
+                    </Link> 
                  </CardBody>   
                 
             </Card>  
-            </Link> 
+            
         );
     }
 
@@ -22,7 +29,7 @@ import { Link } from 'react-router-dom';
 
         const gallery = props.buses.map((bus) => {
             return (
-                <div key={ bus.id } className="col-12 col-md-4 gallery-image">    
+                <div key={ bus.id } className="col-12 col-md-4 gallery-image flip-card">    
                     <RenderBusItem bus={bus} />
                 </div>
             );
